73268ac4bc9512bcec8429b7fa82636e6adcdfd8
Abdulkadir Abdi <abdulkadirabdi7001@gmail.com>
2024-02-13 04:23:50 -0600

    uploaded

---
+++ lab_2.java
@@ -0,0 +1,103 @@
+public class lab_2 {
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        for (int x =0 ; x < 10 ; x++) {
+            System.out.println(x);
+        }
+    }
+
+}
+
+
+public class HelloForLoop {
+
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println(x);
+        }
+
+        // ____ Java Loops and Arrays ____
+
+        public class HelloForLoop {
+
+            public static void main(String[] args) {
+                for (int x = 0; x < 10; x++) {
+                    System.out.println(x);
+                }
+
+
+                // ____Java Loops For Loops____
+                public class Main {
+                    public static void main(String[] args) {
+                        System.out.println("Here are all the dates in January");
+                        for (int day = 1 ; day <= 31 ; day++) {
+                            System.out.println(“January " + day);
+                        }
+                    }
+
+
+                    public class Janvary {
+                        public static void main(String[] args) {
+                            for (int day = 1; day <= 31; day++) {
+                                if (day == 1 || day == 21 || day == 31) {
+                                    System.out.printin(“Janvary * + day + "st");
+                                }
+                               // else if ... for 2nd
+                               // else if ... for 3rd
+                               else {
+                                    System.out.printn(“Janvary * + day);
+                                }
+
+
+
+                                public class 0ilChange {
+                                    public static void main(String[] args) {
+                                        int mileage = 150006;
+                                        for (int oilChange = @ ; oilChange < 8; oilChange++) {
+                                            mileage = mileage + interval;
+                                            System.out.println(“Get oil change at " + mileage + " miles");
+                                        }
+                                    }
+
+
+                                    import static 1nput.inputUtils.doublelnput; 
+                                    public class AverageTestScoreSimpleLoop {
+                                        public static void main(String[] args) {
+                                       //Calculate the average of 5 test scores
+                                            double testScore;
+                                            double testScoreSum = 0.9;
+                                            double testScoreAverage;
+                                            for (int test = 1 ; test <= numberOfTests ; test++) {
+                                                testScore = doubleInput(“Enter test score #" + test);
+                                                        testScoreSum = testScoreSum + testScore;
+                                            }
+
+
+
+                                            // Create a string array, initialized with data
+
+                                            String(] classrooms = { "T3010", “T3020", "T3030", "T3040", "T3050", “T3080"};
+                                            //Use a loop to print all of the data from the array
+
+                                            //Notice how the loop counter is used
+
+                                            for (dnt x =0 ; x <6 ; x++) {
+
+                                                Systenutprintin(classroons x1);
+
+
+                                                // Example int array
+                                                int[] AURbERS = new int[s];
+                                                numbers[8] = 4;
+                                                numbers[1] = 100;
+                                                numbers[5] = 3;
+                                                String[] classNames = { "Java", "C#", "Web"};
+                                                classNames(0] = "Java";
+                                                classNames[1] = "C#";
+                                                classNames[2] = "Web";
+                                                System.out.println(“One of the second year classes is " + classNames[2]);
+                                                int number0fClasses = classNames.length;
+                                                System.out.println(“There are * + numberOfClasses + “ classes.");
