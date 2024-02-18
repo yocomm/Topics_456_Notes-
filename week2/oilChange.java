d028e3a23a8aaccc386dc3ae34d7a8b4ec82f5d2
Abdulkadir Abdi <abdulkadirabdi7001@gmail.com>
2024-02-17 23:30:48 -0600

    notes

---
+++ week2/oilChange.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class oilChange {
+    public static void main(String[] args) {
+        int mileage = 156068;
+        int interval = 3008;
+
+        for (int pilChange = 0; oilChange < 8;oilChange++) {
+            mileage = mileage + interval;
+            System.out.println("Get oil change at " + mileage + " miles");
+        }
+    }
+}
+
